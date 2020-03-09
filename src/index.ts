36082e9cbb49462c8b9f5ecdfa6ceb0918212c07
devminchan <minchan.dev@gmail.com>
2020-03-09 10:47:22 +0900

    Typeorm 연동

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
 import Koa from 'koa';
 import apiRouter from './api';
 import bodyparser from 'koa-bodyparser';
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
 
-const app = new Koa();
+createConnection()
+  .then(async () => {
+    console.log('Connect to database... OK');
+    const app = new Koa();
 
-app.use(bodyparser());
+    app.use(bodyparser());
 
-app.use(apiRouter.allowedMethods());
-app.use(apiRouter.routes());
+    app.use(apiRouter.allowedMethods());
+    app.use(apiRouter.routes());
 
-app.listen(4000, () => {
-  console.log('Listening on port 4000');
-});
+    app.listen(4000, () => {
+      console.log('Listening on port 4000');
+    });
+  })
+  .catch(error => console.log(error));

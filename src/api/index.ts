7081667a44f53f9abea430f5836df8ba31543786
devminchan <minchan.dev@gmail.com>
2020-03-10 22:36:22 +0900

    jwt 인증 미들웨어 추가

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,6 +2,7 @@ import Router from '@koa/router';
 
 import userRouter from './users';
 import authRouter from './auth';
+import { Context } from 'koa';
 
 const router = new Router();
 
@@ -10,5 +11,12 @@ router.use(authRouter.routes());
 
 router.use(userRouter.allowedMethods());
 router.use(userRouter.routes());
+
+// test endpoint
+router.get('/hello', (ctx: Context) => {
+  ctx.body = {
+    message: 'Success!!',
+  };
+});
 
 export default router;
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,37 @@
 import 'dotenv/config';
 
-import Koa from 'koa';
+import Koa, { Next, Context } from 'koa';
 import apiRouter from './api';
 import bodyparser from 'koa-bodyparser';
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
+import koaJwt from 'koa-jwt';
 
 createConnection()
   .then(async () => {
     console.log('Connect to database... OK');
     const app = new Koa();
 
+    // error handler
+    app.use(async (ctx: Context, next: Next) => {
+      try {
+        await next();
+      } catch (e) {
+        console.error(e);
+
+        ctx.status = e.status || 500;
+        ctx.body = {
+          message: e.message,
+        };
+      }
+    });
+
     app.use(bodyparser());
+    app.use(
+      koaJwt({ secret: process.env.JWT_SECRET }).unless({
+        path: [/users\/facebook/, /auth*/],
+      }),
+    );
 
     app.use(apiRouter.allowedMethods());
     app.use(apiRouter.routes());

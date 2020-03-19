2ae1d0b459c051622546f3e6da72e1c9946d8aad
devminchan <minchan.dev@gmail.com>
2020-03-19 15:36:06 +0900

    ormconfig port 부분 환경 변수 string -> number로 파싱 적용

--- ormconfig.js
+++ ormconfig.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === 'production') {
 module.exports = {
   type: 'mysql',
   host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 3306,
+  port: Number.parseInt(process.env.DB_PORT) || 3306,
   username: process.env.DB_USERNAME || 'root',
   password: process.env.DB_PASSWORD || '1234',
   database: process.env.DB_DBNAME || 'test',

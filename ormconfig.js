b648d2ea73b3b058d766e6863c655709707a204a
devminchan <minchan.dev@gmail.com>
2020-03-19 23:43:05 +0900

    ormconfig.js 수정 (DB type 변경을 위함)

--- ormconfig.js
+++ ormconfig.js
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 module.exports = {
-  type: 'mysql',
+  type: process.env.DB_TYPE || 'mysql',
   host: process.env.DB_HOST || 'localhost',
   port: Number.parseInt(process.env.DB_PORT) || 3306,
   username: process.env.DB_USERNAME || 'root',

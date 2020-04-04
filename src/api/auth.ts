80fd85ea177b09e1616714e30a1f08b09be7b367
devminchan <minchan.dev@gmail.com>
2020-04-04 17:05:32 +0900

    FacebookUser API 제거
    FacebookUser 엔티티 제거
    FacebookUser 관련 인증, 생성 API 제거

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,87 +1,34 @@
 import Router from '@koa/router';
 import { getRepository } from 'typeorm';
-import { FacebookUser } from '@/entity/FacebookUser';
-import {
-  FacebookLoginRequest,
-  FacebookLoginResponse,
-  GRAPH_API_URL,
-} from './users';
-import axios, { AxiosResponse } from 'axios';
-import QueryString from 'query-string';
 import JWT from 'jsonwebtoken';
 import { EmailUser } from '@/entity/EmailUser';
 
 const router = new Router();
 
-router
-  .post('/auth/email', async ctx => {
-    const emailUserRepository = getRepository(EmailUser);
-    const { email, password } = ctx.request.body;
+router.post('/auth/email', async ctx => {
+  const emailUserRepository = getRepository(EmailUser);
+  const { email, password } = ctx.request.body;
 
-    const user = await emailUserRepository.findOne({
-      email,
-      password,
-    });
-
-    const jwt = JWT.sign(
-      {
-        id: user.id,
-        email: user.email,
-      },
-      process.env.JWT_SECRET,
-      {
-        expiresIn: '7d',
-        algorithm: 'HS512',
-      },
-    );
-
-    ctx.body = {
-      token: jwt,
-    };
-  })
-  .post('/auth/facebook', async ctx => {
-    const facebookUserRepository = getRepository(FacebookUser);
-    const { token } = ctx.request.body;
-
-    const loginRequest = {
-      // eslint-disable-next-line @typescript-eslint/camelcase
-      access_token: token,
-      fields: 'id,name,email',
-    } as FacebookLoginRequest;
-
-    const qs = QueryString.stringify(loginRequest);
-
-    let response: AxiosResponse<FacebookLoginResponse>;
-    let resData: FacebookLoginResponse;
-
-    // Facebook access token을 통한 검증
-    // access token이 잘못되었을 시 400 error 발생
-    try {
-      response = await axios.get(GRAPH_API_URL + 'me?' + qs);
-      resData = response.data;
-    } catch (e) {
-      ctx.throw(400, e);
-    }
-
-    const user = await facebookUserRepository.findOneOrFail({
-      facebookUserId: resData.id,
-    });
-
-    const jwt = JWT.sign(
-      {
-        id: user.id,
-        email: user.email,
-      },
-      process.env.JWT_SECRET,
-      {
-        expiresIn: '7d',
-        algorithm: 'HS512',
-      },
-    );
-
-    ctx.body = {
-      token: jwt,
-    };
+  const user = await emailUserRepository.findOne({
+    email,
+    password,
   });
+
+  const jwt = JWT.sign(
+    {
+      id: user.id,
+      email: user.email,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: '7d',
+      algorithm: 'HS512',
+    },
+  );
+
+  ctx.body = {
+    token: jwt,
+  };
+});
 
 export default router;
--- src/api/users.ts
+++ src/api/users.ts
@@ -1,30 +1,9 @@
 import Router from '@koa/router';
-import axios, { AxiosResponse } from 'axios';
-import QueryString from 'query-string';
 import { getRepository } from 'typeorm';
-import { FacebookUser } from '@/entity/FacebookUser';
 import { EmailUser } from '@/entity/EmailUser';
-import { Readable } from 'stream';
-import fs from 'fs';
-import mime from 'mime';
 import { User } from '@/entity/User';
 import jwtValidate from '@/middleware/jwt-validate';
 import { validateOrReject, MinLength } from 'class-validator';
-
-export interface FacebookLoginRequest {
-  access_token: string;
-  fields: string;
-}
-
-export interface FacebookLoginResponse {
-  id: string;
-  email: string;
-  name: string;
-}
-
-export const GRAPH_API_URL = 'https://graph.facebook.com/';
-
-const RESOURCE_PATH = 'resources/';
 
 const router = new Router();
 
@@ -61,70 +40,6 @@ router
 
     ctx.body = {
       ...newUser,
-    };
-  })
-  .post('/users/facebook', async ctx => {
-    const facebookUserRepository = getRepository(FacebookUser);
-
-    const { token } = ctx.request.body;
-
-    const loginRequest: FacebookLoginRequest = {
-      // eslint-disable-next-line @typescript-eslint/camelcase
-      access_token: token,
-      fields: 'id,email,name',
-    };
-
-    const qs = QueryString.stringify(loginRequest);
-
-    const saveImageToLocal = async (
-      url: string,
-      imagePath: string,
-    ): Promise<string> => {
-      const streamRes = (await axios({
-        url,
-        responseType: 'stream',
-      })) as AxiosResponse<Readable>;
-
-      return new Promise((resolve, reject) => {
-        if (!fs.existsSync(RESOURCE_PATH)) {
-          fs.mkdirSync(RESOURCE_PATH);
-        }
-
-        const ext = mime.getExtension(streamRes.headers['content-type']);
-        const filePath = RESOURCE_PATH + imagePath + '.' + ext; // <fileName>.jpg
-
-        streamRes.data
-          .pipe(fs.createWriteStream(filePath))
-          .on('finish', () => {
-            resolve(filePath);
-          })
-          .on('error', e => {
-            reject(e);
-          });
-      });
-    };
-
-    const response = await axios.get(GRAPH_API_URL + 'me?' + qs);
-    const data = response.data as FacebookLoginResponse;
-
-    const downloadUrl = GRAPH_API_URL + `${data.id}/` + 'picture?type=large';
-    const saveResult = await saveImageToLocal(downloadUrl, data.id);
-
-    console.log('file saved as ' + saveResult);
-
-    const user = facebookUserRepository.create({
-      email: data.email,
-      username: data.name,
-      facebookUserId: data.id,
-      profileImage: saveResult,
-    });
-
-    await user.save();
-
-    console.log(`User ${user.email} is now registered`);
-
-    ctx.body = {
-      ...user,
     };
   })
   .get('/users/me', jwtValidate(), async ctx => {

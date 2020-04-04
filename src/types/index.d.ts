b8daee976a4ae49d333ffe67494249262ed2d607
devminchan <minchan.dev@gmail.com>
2020-04-04 19:59:42 +0900

    ProcessEnv interface 추가

---
+++ src/types/index.d.ts
@@ -0,0 +1,11 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: string;
+    DB_HOST: string;
+    DB_USER: string;
+    DB_PASSWORD: string;
+    DB_DBNAME: string;
+    DB_PORT: string | number;
+    JWT_SECERT: string;
+  }
+}

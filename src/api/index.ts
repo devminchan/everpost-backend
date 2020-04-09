93facae260e72e9edbcc53e1e4b9561ec2d69629
devminchan <minchan.dev@gmail.com>
2020-04-09 16:42:18 +0900

    인증 기능 처리를 auth.ts에서 users.ts로 옮김
    user.ts 제거

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,14 +1,10 @@
 import Router from '@koa/router';
 
 import userRouter from './users';
-import authRouter from './auth';
 import postRouter from './posts';
 import uploadRouter from './upload';
 
 const router = new Router();
-
-router.use(authRouter.allowedMethods());
-router.use(authRouter.routes());
 
 router.use(userRouter.allowedMethods());
 router.use(userRouter.routes());
--- src/api/users.ts
+++ src/api/users.ts
@@ -5,6 +5,7 @@ import { validateOrReject, MinLength } from 'class-validator';
 import { Post } from '@/entity/Post';
 import { PasswordAccountAccess } from '@/entity/PasswordAccountAccess';
 import bcrypt from 'bcrypt';
+import JWT from 'jsonwebtoken';
 
 const router = new Router();
 
@@ -58,6 +59,40 @@ router
       await newUser.remove();
       throw e;
     }
+  })
+  .post('/users/auth', async ctx => {
+    const { email, password } = ctx.request.body;
+
+    const user = await User.findOneOrFail({
+      email,
+    });
+
+    const acc = await PasswordAccountAccess.findOneOrFail({
+      user,
+    });
+
+    // 패스워드 불일치 시
+    const isSuccess = await bcrypt.compare(password, acc.password);
+
+    if (!isSuccess) {
+      ctx.throw(401, 'Password is wrong');
+    }
+
+    const jwt = JWT.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: '7d',
+        algorithm: 'HS512',
+      },
+    );
+
+    ctx.body = {
+      token: jwt,
+    };
   })
   .get('/users/me', jwtValidate(), async ctx => {
     const { id } = ctx.state.user;

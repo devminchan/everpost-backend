66ba76760cfc7637a10cd0db1ca530c60cca6dda
devminchan <minchan.dev@gmail.com>
2020-04-20 17:11:43 +0900

    ProcessEnv interface 수정

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,7 +2,7 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: string;
     DB_HOST: string;
-    DB_USER: string;
+    DB_USERNAME: string;
     DB_PASSWORD: string;
     DB_DBNAME: string;
     DB_PORT: string;
